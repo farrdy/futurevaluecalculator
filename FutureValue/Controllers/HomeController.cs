d96453be5d6804c35b56b826f1e1cbd8a321c92b
Farai Motsi <farai.motsi@gmail.com>
2021-02-15 22:07:49 +0200

    first commit

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            ViewBag.FutureValue = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+
+            if ( ModelState.IsValid)
+            {
+                ViewBag.FutureValue = fv.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.FutureValue = "";
+
+            }
+            return View();
+        }
+
+    }
+}
