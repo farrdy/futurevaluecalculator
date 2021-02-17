177ea5af9f4f72716e5928d87d48a3b70eb2729a
Farai Motsi <farai.motsi@gmail.com>
2021-02-17 20:11:13 +0200

    demo 1

--- FutureValue/Models/FutureValueModel.cs
+++ FutureValue/Models/FutureValueModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,22 +9,28 @@ namespace FutureValue.Models
     public class FutureValueModel
     {
 
-        public decimal MonthlyInvestment { get; set; }
+        [Required(ErrorMessage = "Please enter a monthly investment")]
+        [Range(1,500,ErrorMessage ="Monthly investment must be between 1 and 500")]
+        public decimal? MonthlyInvestment { get; set; }
 
-        public decimal YearlyInterestRate { get; set; }
-        public int Years { get; set; }
+        [Required(ErrorMessage = "Please enter Yearly  InterestRate")]
+        [Range(0.1, 10.0, ErrorMessage = "Yearly interest rate must be between 0.1 and 10.0")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter Years of investment")]
+        [Range(1, 50, ErrorMessage = "Years  must be between 1 and 50")]
+        public int? Years { get; set; }
 
         public decimal Calculate() {
 
 
-            int months = Years * 12;
-            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            int months = Years.Value * 12;
+            decimal monthlyInterestRate = YearlyInterestRate.Value / 12 / 100;
             decimal futureValue = 0;
 
             for (int i = 0; i < months; i++)
             {
-                futureValue
-                    = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+                futureValue = (futureValue + MonthlyInvestment.Value) * (1 + monthlyInterestRate);
             }
             return futureValue;
 
